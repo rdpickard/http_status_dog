a4212fa81c55e14a785f684f0288b73ddf4c6d7a
Robert Daniel Pickard <robert.daniel.pickard@chalkfarm.org>
2020-01-31 19:11:15 -0500

    Basic producer and consumer code

--- https_status_dog.py
+++ https_status_dog.py
@@ -3,9 +3,11 @@ from werkzeug.utils import secure_filename
 
 application = flask.Flask(__name__)
 
+
 @application.before_first_request
 def pre_first_request():
     pass
+
 
 @application.route('/')
 def index():
@@ -13,7 +15,8 @@ def index():
     Returns the "index" page. Source of page is in templates/index.jinja2
     :return:
     """
-    return flask.render_template('index.jinja2')
+    return flask.render_template('index.jinja2',
+                                 my_server=flask.request.url_root)
 
 
 @application.route("/<string:status_code>")
@@ -28,4 +31,3 @@ def status_dog(status_code):
 
 if __name__ == "__main__":
     application.run()
-
